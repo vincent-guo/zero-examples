be281970cd2ece71b66333ef1a9051a9cdaca168
vincent-guo <vincent.weiguo@gmail.com>
2021-10-29 15:03:47 +0800

    generate rpc

---
+++ bookstore/rpc/check/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "bookstore/rpc/check/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
