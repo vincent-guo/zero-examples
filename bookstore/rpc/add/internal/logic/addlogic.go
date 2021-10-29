0ba5d21919fa77c7b385f197c26bd100f412f0a9
vincent-guo <vincent.weiguo@gmail.com>
2021-10-29 14:20:14 +0800

    generate rpc

---
+++ bookstore/rpc/add/internal/logic/addlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"bookstore/rpc/add/add"
+	"bookstore/rpc/add/internal/svc"
+
+	"github.com/tal-tech/go-zero/core/logx"
+)
+
+type AddLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewAddLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddLogic {
+	return &AddLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *AddLogic) Add(in *add.AddReq) (*add.AddResp, error) {
+	// todo: add your logic here and delete this line
+
+	return &add.AddResp{}, nil
+}
---
+++ bookstore/rpc/add/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "bookstore/rpc/add/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
