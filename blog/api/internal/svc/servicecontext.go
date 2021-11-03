e8b5ff97cdd1f27d618f6dae13d62d92dd0963e5
vincent-guo <vincent.weiguo@gmail.com>
2021-11-03 21:02:34 +0800

    update servicecontext.go

--- blog/api/internal/svc/servicecontext.go
+++ blog/api/internal/svc/servicecontext.go
@@ -2,14 +2,19 @@ package svc
 
 import (
 	"blog/api/internal/config"
+	"blog/rpc/user/users"
+
+	"github.com/tal-tech/go-zero/zrpc"
 )
 
 type ServiceContext struct {
 	Config config.Config
+	User   users.Users
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
+		User:   users.NewUsers(zrpc.MustNewClient(c.User)),
 	}
 }
